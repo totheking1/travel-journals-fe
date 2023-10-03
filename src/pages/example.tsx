98b0ae2f20f3e3e270ae0fcdfd2627e44c2f69f8
Yejin Jin <yeen0606@gmail.com>
2023-10-03 20:54:54 +0900

    linting test

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Unstable_Grid2";
 
 export default function Example() {
-  return <Button variant="contained">Hello world</Button>;
   return (
-    <Button variant="contained" href="/">
-      홈으로
-    </Button>
+    <Grid container>
+      <Grid xs={12} sm={12} md={12} xl={12}>
+        <Button variant="contained">Hello world</Button>
+      </Grid>
+      <Grid xs={12} sm={12} md={12} xl={12}>
+        <Button variant="contained" href="/">
+          홈으로
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
