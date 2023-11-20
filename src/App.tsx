a5ed4b2d8aeebea47b16227ab901a835004564a7
Yejin Jin <yeen0606@gmail.com>
2023-11-20 22:56:52 +0900

    update a AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./App.css";
-import Layout from "./components/Layout";
+import { Outlet } from "react-router-dom";
+import Grid from "@mui/material/Unstable_Grid2";
+import MyAppBar from "./components/Layout/MyAppBar";
 
 function App() {
-  return <Layout />;
+  return (
+    <Grid container>
+      <Grid xs={12} sm={12} md={12} xl={12}>
+        <MyAppBar />
+        <Outlet />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
