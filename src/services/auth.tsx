4a62340a8dc451f9b54ffc64f4b071df86bae2d0
myim10 <miyim@DESKTOP-F74082E.localdomain>
2023-12-06 22:09:42 +0900

    Fix error

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { Buffer } from "buffer";
 
 const loginUrl = "http://localhost:8080";
 
@@ -10,10 +11,8 @@ export interface LoginRequest {
 export const login = async (
   loginRequest: LoginRequest,
 ): Promise<AxiosResponse<void>> => {
-  const token: string = Buffer.from(
-    `${loginRequest.id}:${loginRequest.password}`,
-    "base64",
-  ).toString();
+  const data: string = `${loginRequest.id}:${loginRequest.password}`;
+  const token: string = Buffer.from(data).toString("base64");
   const config: AxiosRequestConfig = {
     headers: {
       Authorization: `Basic ${token}`,
